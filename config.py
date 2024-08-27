a2eafee3c231fb63ff0ad27ce93f461a142cab23
Kaizen-san <136605818+Kaizen-san@users.noreply.github.com>
2024-08-27 10:59:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
 
-class Config:
-    TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+print("Direct TELEGRAM_TOKEN:", TELEGRAM_TOKEN)
+
+if not TELEGRAM_TOKEN:
+    raise ValueError("No TELEGRAM_TOKEN set for Bot")
