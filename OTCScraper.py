3bad1188f99b08d5c67af01b5b081bb772151a57
Michael <michaelkrizi@gmail.com>
2024-08-29 22:23:39 +0300

    a

--- OTCScraper.py
+++ OTCScraper.py
@@ -9,7 +9,7 @@ from telegram.constants import ParseMode
 import gspread
 from google.oauth2.service_account import Credentials
 
-# Set up logging
+# Set up loggingg
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG
