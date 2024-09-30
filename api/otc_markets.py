daa4d0d9568af9e8e97980824817bbf92f73103e
Kaizen-san <136605818+Kaizen-san@users.noreply.github.com>
2024-09-30 12:46:23 +0300

    get profile func edit

--- api/otc_markets.py
+++ api/otc_markets.py
@@ -8,6 +8,15 @@ BASE_URL = "https://backend.otcmarkets.com/otcapi"
 
 async def get_profile_data(ticker):
     url = f"{BASE_URL}/company/profile/full/{ticker}"
+    headers = {
+        "Host": "backend.otcmarkets.com",
+        "Origin": "https://www.otcmarkets.com",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Connection": "keep-alive",
+        "Referer": "https://www.otcmarkets.com/",
+    }
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers=headers) as response:
             response.raise_for_status()
